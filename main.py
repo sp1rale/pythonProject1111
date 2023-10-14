22f3394059546d011085f3b329d6b861fba1763e
sp1rale <dimayaremchyk24@gmail.com>
2023-10-14 19:14:07 +0300

    Ex-5 task was done

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_product(start,end):
-    if start>end:
-        start,end = end,start
-
-        product = 1
-        for num in range(start,end+1):
-            product *= num
-            return product
-        result = calculate_product(5,25)
-        print(result)
+def count_digits(number):
+    return len(str(number))
+num = 3456
+digit_count = count_digits(num)
+print(digit_count)
