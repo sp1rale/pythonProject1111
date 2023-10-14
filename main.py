830f69a65c83b664caed83ef0ae24b716c0b6929
sp1rale <dimayaremchyk24@gmail.com>
2023-10-14 19:03:15 +0300

    Ex-3 task was done

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def display_square(side_length,symbol,filled):
-    if filled:
-        for i in range(side_length):
-            print(symbol * side_length)
-        else:
-            print(symbol * side_length)
-            for i in range(side_length -2):
-                print(symbol + ''*(side_length-2)+symbol)
-                print(symbol * side_length)
-
-                display_square(5,'*',False)
-                display_square(5,'*',True)
+def find_min(a,b,c,d,e):
+    return min(a,b,c,d,e)
+min_value = find_min(3,7,1,9,2)
+print(min_value)
