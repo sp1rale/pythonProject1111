12a3df068152190433200c87acfe77cb5f01a1ce
sp1rale <dimayaremchyk24@gmail.com>
2023-10-14 19:21:13 +0300

    Ex-7 task was done

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_digits(number):
-    return len(str(number))
-num = 3456
-digit_count = count_digits(num)
-print(digit_count)
+def is_palindrome(number):
+    num_str = str(number)
+    return num_str == num_str[::-1]
+result1 = is_palindrome(123321)
+result2 = is_palindrome(546645)
+result3 = is_palindrome(421987)
+
+print(result1) #True
+print(result2) #True
+print(result3) #False
