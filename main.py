45d35970fc8032c8a59e618d12f332c220c2a316
sp1rale <dimayaremchyk24@gmail.com>
2023-10-25 15:39:14 +0300

    Ex-1 task was done

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_palindrome(number):
-    num_str = str(number)
-    return num_str == num_str[::-1]
-result1 = is_palindrome(123321)
-result2 = is_palindrome(546645)
-result3 = is_palindrome(421987)
+def display_odd_numbers_between(start, end):
+    for number in range(start + 1, end):
+        if number % 2 != 0:
+            print(number)
 
-print(result1) #True
-print(result2) #True
-print(result3) #False
+display_odd_numbers_between(1, 10)
