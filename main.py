a3b54806752dea001ab5e9f455e36130d6b05d5b
sp1rale <dimayaremchyk24@gmail.com>
2023-10-25 15:49:12 +0300

    Ex-5 task was done

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-def calculate_sum_in_range(start, end):
-    total_sum = 0
-    for number in range(start, end + 1):
-        total_sum += number
-    return total_sum
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
 
-start = 1
-end = 10
-result = calculate_sum_in_range(start, end)
-print(f"Сума чисел в діапазоні від {start} до {end} дорівнює {result}")
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+num = 17
+if is_prime(num):
+    print(f"{num} є простим числом.")
+else:
+    print(f"{num} не є простим числом.")
