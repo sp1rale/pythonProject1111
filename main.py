826a165d01566d7db5cf6d8395261b0c7504a67b
sp1rale <dimayaremchyk24@gmail.com>
2023-10-25 15:52:13 +0300

    Ex-6 task was done

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-def is_prime(number):
-    if number <= 1:
-        return False
-    if number <= 3:
-        return True
-    if number % 2 == 0 or number % 3 == 0:
+def is_lucky_number(number):
+    if len(str(number)) != 6:
         return False
 
-    i = 5
-    while i * i <= number:
-        if number % i == 0 or number % (i + 2) == 0:
-            return False
-        i += 6
+    first_half = number // 1000
+    second_half = number % 1000
 
-    return True
+    sum_first_half = sum(map(int, str(first_half)))
+    sum_second_half = sum(map(int, str(second_half)))
 
-num = 17
-if is_prime(num):
-    print(f"{num} є простим числом.")
+    return sum_first_half == sum_second_half
+
+num = 123420
+if is_lucky_number(num):
+    print(f"{num} є щасливим числом.")
 else:
-    print(f"{num} не є простим числом.")
+    print(f"{num} не є щасливим числом.")
